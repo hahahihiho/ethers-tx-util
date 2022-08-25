5d5c2a4578bd3d1a2abd85b5ae97633bc0a9c5c4
hahahihiho <hymy@coinplug.com>
2022-08-25 18:40:14 +0900

    refactoring index.js, add feature isConnected on provider, update testcode

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ethers = require('ethers');
 const path = require("path")
 
 const PRE_PATH = path.join(__dirname,"../../")
-const CONFIG_PATH = path.join(PRE_PATH,"./config")
+// const CONFIG_PATH = path.join(PRE_PATH,"./config")
 
 function _read_json(path){
     const data = fs.readFileSync(path);
@@ -12,32 +12,10 @@ function _read_json(path){
 }
 
 /**
- * Which path does it return
  * 
- * truffle : contract_path == undefined 
- * hardhat :
- *  - @openzeppelin : contract_path strats with @openzeppelin
- *  - normal contract_path
+ * configUtils
+ * 
  */
-function _getCompiledInfoPath(contract_path,contract_name){
-    // handle post-fix ".sol"
-    if(contract_path!=undefined && contract_path.slice(-4) == ".sol") contract_path = contract_path.slice(0,contract_path.length-".sol".length)
-    const truffle_path = path.join(PRE_PATH,`./build/contracts/${contract_name}.json`)
-    const hardhat_path = path.join(PRE_PATH,`./artifacts/contracts/${contract_path}.sol/${contract_name}.json`)
-    const hardhat_package_path = path.join(PRE_PATH,`./artifacts/${contract_path}.sol/${contract_name}.json`)
-    // @openzeppelin, @uniswap,,,
-    if(contract_path != undefined && contract_path.slice(0,1) === "@" && fs.existsSync(hardhat_package_path)){
-        return hardhat_package_path;
-    } else if(contract_path == undefined && fs.existsSync(truffle_path)) return truffle_path;
-    else return hardhat_path
-}
-
-
-function get_contract_addresses(){
-    const file_path = path.join(PRE_PATH ,"./config/contract_addresses.js");
-    const CAs = require(file_path);
-    return CAs
-}
 
 function get_additional_info(what){
     const file_path = path.join(PRE_PATH ,`./config/${what}_info.js`);
@@ -49,65 +27,31 @@ function get_additional_info(what){
     }
 }
 
-function get_accounts(){
-    const file_path = path.join(PRE_PATH,"./config/accounts.json");
-    if(!fs.existsSync(file_path)){
-        const accounts = []
+function get_pk(){
+    let pks = ""
+    try{
         require("dotenv").config({path:path.join(PRE_PATH,"./.env")})
-        const pks = process.env.PK_LIST || "";
-        for(let pk of pks.split(",")){
-            accounts.push(new ethers.Wallet(pk).address)
-        }
-        fs.writeFileSync(file_path,JSON.stringify(accounts))
+    } catch(err){
+        pks = process.env.PK_LIST || "";
+    } finally{
+        return pks.split(",").filter(pk=>pk.length!=0);
     }
-    return _read_json(file_path)
 }
 
-function get_pk(){
-    require("dotenv").config({path:path.join(PRE_PATH,"./.env")})
-    const pks = process.env.PK_LIST || "";
-    return pks.split(",");
+function get_accounts(){
+    const accounts = []
+    const pks = get_pk();
+    for(let pk of pks){
+        accounts.push(new ethers.Wallet(pk).address)
+    }
+    return accounts
 }
+
 
 function get_signer(url,priv_k){
     const provider = new ethers.providers.JsonRpcProvider(url)
     const signer = new ethers.Wallet(priv_k,provider)
     return signer
-}
-
-// function getContract(ca,abi,signer_or_provider){
-//     let contract = new ethers.Contract( ca , abi , signer_or_provider)
-//     return contract
-// }
-
-// function change_signer(contract,priv_k,provider){
-//     const signer = new ethers.Wallet(priv_k,provider)
-//     return contract.connect(signer)
-// }
-
-// function change_contract_addr(contract,contract_address){
-//     return contract.attach(contract_address)
-// }
-
-async function deployContract(contract_path,contract_name,signer,[...args]){
-    const info = getInfo(contract_path,contract_name)
-    const abi = info.abi;
-    const bytecode = info.bytecode;
-    
-    const ca = {}
-    const factory = new ethers.ContractFactory(abi, bytecode, signer)
-    const contract = await factory.deploy(...args)
-    ca[contract_name] = contract.address
-    writeCA(ca)
-    console.log("deploy",contract_name,"on",contract.address)
-    return contract
-}
-
-function getInfo(contract_path,contract_name){
-    const info_path = _getCompiledInfoPath(contract_path,contract_name)
-    let info = fs.readFileSync(info_path)
-    info = JSON.parse(info.toString());
-    return info
 }
 
 function readCA(injected_path){
@@ -130,10 +74,64 @@ function writeCA(ca){
     fs.writeFileSync(ca_path,JSON.stringify(ca_new))
 }
 
+/**
+ * 
+ *  hardhatUtils
+ * 
+ */
+
+/**
+ * Which path does it return
+ * 
+ * truffle : contract_path == undefined 
+ * hardhat :
+ *  - @openzeppelin : contract_path strats with @openzeppelin
+ *  - normal contract_path
+ */
+ function _getCompiledInfoPath(contract_path,contract_name){
+    // handle post-fix ".sol"
+    if(contract_path!=undefined && contract_path.slice(-4) == ".sol") contract_path = contract_path.slice(0,contract_path.length-".sol".length)
+    const truffle_path = path.join(PRE_PATH,`./build/contracts/${contract_name}.json`)
+    const hardhat_path = path.join(PRE_PATH,`./artifacts/contracts/${contract_path}.sol/${contract_name}.json`)
+    const hardhat_package_path = path.join(PRE_PATH,`./artifacts/${contract_path}.sol/${contract_name}.json`)
+    // @openzeppelin, @uniswap,,,
+    if(contract_path != undefined && contract_path.slice(0,1) === "@" && fs.existsSync(hardhat_package_path)){
+        return hardhat_package_path;
+    } else if(contract_path == undefined && fs.existsSync(truffle_path)) return truffle_path;
+    else return hardhat_path
+}
+
+function getInfo(contract_path,contract_name){
+    const info_path = _getCompiledInfoPath(contract_path,contract_name)
+    let info = fs.readFileSync(info_path)
+    info = JSON.parse(info.toString());
+    return info
+}
+
+async function deployContract(contract_path,contract_name,signer,[...args]){
+    const info = getInfo(contract_path,contract_name)
+    const abi = info.abi;
+    const bytecode = info.bytecode;
+    
+    const ca = {}
+    const factory = new ethers.ContractFactory(abi, bytecode, signer)
+    const contract = await factory.deploy(...args)
+    ca[contract_name] = contract.address
+    writeCA(ca)
+    console.log("deploy",contract_name,"on",contract.address)
+    return contract
+}
+
 async function getImpersonatedSigner(provider,address){
     await provider.send("hardhat_impersonateAccount",[address])
     return provider.getSigner(address);
 }
+
+/**
+ * 
+ *  txUtils
+ * 
+ */
 
 function makeRawTxData(functionInterface,parameters){
     const signature = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(functionInterface)).slice(0,10)
@@ -142,28 +140,113 @@ function makeRawTxData(functionInterface,parameters){
     return signature + data;
 }
 
+/**
+ * 
+ *  ethersUtils
+ * 
+ */
+
+// function getContract(ca,abi,signer_or_provider){
+//     let contract = new ethers.Contract( ca , abi , signer_or_provider)
+//     return contract
+// }
+
+// function change_signer(contract,priv_k,provider){
+//     const signer = new ethers.Wallet(priv_k,provider)
+//     return contract.connect(signer)
+// }
+
+// function change_contract_addr(contract,contract_address){
+//     return contract.attach(contract_address)
+// }
+
+/**
+ *  ethers provider with isConnected function
+ */
+class ProviderModule{
+    constructor(url){
+        this.url = url;
+        this.protocol_type = url.split(":")[0]
+        this.provider = this.getProvider();
+        this.bindingProperties()
+        return this.provider
+    }
+    bindingProperties(){
+        const properties = ["url","protocol_type","isWs","isHttp","getProvider","isConnected","closeConnection"]
+        for(const p of properties){
+            this.provider[p] = eval("this."+p);
+        }
+    }
+
+    isWs(){
+        return this.protocol_type.includes("ws")
+    }
+    isHttp(){
+        return this.protocol_type.includes("http")
+    }
+
+    getProvider(){
+        if(this.isHttp()){
+            return new ethers.providers.JsonRpcProvider(this.url);
+        } else if(this.isWs()){
+            const provider = new ethers.providers.WebSocketProvider(this.url)
+            provider.websocket.on("error",()=>{})
+            return provider;
+        } else {
+            throw `unexpected protocol_type : ${this.protocol_type}`
+        }
+    }
+
+    async isConnected(){
+        let connection = false;
+        try{
+            if(this.isHttp()){
+                const http = new ethers.providers.JsonRpcProvider(this.url);
+                await http.detectNetwork();
+                connection = true
+            } else if(this.isWs()){
+                const ws = new ethers.providers.WebSocketProvider(this.url);
+                connection = true;
+                ws.websocket.on("error",(err)=> connection=false)
+                await ws.destroy()
+            }
+            return connection;
+        } catch (error) {
+            return false;
+        }
+    }
+    closeConnection(){
+        const provider = this;
+        if(this.isWs() && provider.websocket.readyState == 1){
+            provider.websocket.close();
+        }
+    }
+}
+
 module.exports = {
     // get config
-    configUtil : {
-        get_contract_addresses,
+    configUtils : {
         get_additional_info,
-        get_accounts,
         get_pk,
+        get_accounts,
         get_signer,
         readCA,
         writeCA,
     },
     // get contract
-    hardhatUtil:{
+    hardhatUtils:{
         getInfo,
         deployContract,
         getImpersonatedSigner,
     },
-    // util
-    txUtil:{
+    // utils
+    ethersUtils:{
+        ProviderModule
+        // getContract
+        // change_signer,
+        // change_contract_addr,
+    },
+    txUtils:{
         makeRawTxData
-    }
-    // getContract
-    // change_signer,
-    // change_contract_addr,
+    },
 }
--- test/test_tx_util.js
+++ test/test_tx_util.js
@@ -1,22 +1,30 @@
-const tx_util = require("../tx_util");
-const ethers = require("ethers");
-const hre = require("hardhat")
+const tx_util = require("../");
 
 // PK_ADMIN, PK_LIST must be defined in .env
-async function main(){
-    
+function testConfigUtils(){
     // get pk from .env
-    const pks = tx_util.get_pk()
-    const pk_list = pks.PK_LIST;
-    console.log(pk_list)
+    const pks = tx_util.configUtils.get_pk()
+    console.log(pks)
 
     // get accounts from ../config/accounts.json if it doesn't exist,
     // get account addresses from .env(private key -> address)
-    const accounts = tx_util.get_accounts();
+    const accounts = tx_util.configUtils.get_accounts();
     console.log(accounts);
-    const ca = tx_util.get_contract_addresses();
-    console.log(ca)
+}
 
+async function testEthersUtils(){
+    const url_list = ["http://127.0.0.1:8545","http://127.0.0.1:8546","ws://127.0.0.1:8545","ws://127.0.0.1:8546"]
+    for(const url of url_list){
+        const provider = new tx_util.ethersUtils.ProviderModule(url)
+        const connection = await provider.isConnected();
+        console.log(url,"is connected : ",connection);
+        provider.closeConnection();
+    }
+}
+
+async function main(){
+    testConfigUtils()
+    testEthersUtils()
 }
 
 main();
